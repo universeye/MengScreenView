8bae94394c9c82d77d197e4dfb7070ada73f0b24
Terry <terrykuo501@gmail.com>
2021-04-14 13:01:39 +0800

    add drag animation and readme files

--- MengLogInScreen/View/ContentView.swift
+++ MengLogInScreen/View/ContentView.swift
@@ -12,34 +12,44 @@ struct ContentView: View {
     
     //@State private var rect: CGRect = CGRect()
     @State private var appear = false
+    @State var viewState = CGSize.zero //used for drag animation
     
-    var background = LinearGradient(gradient: Gradient(stops: [
-                                                        .init(color: Color(#colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1)), location: 0),
-                                                        .init(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)), location: 1)]), startPoint: UnitPoint(x: 0.5, y: -3.061671314629196e-17), endPoint: UnitPoint(x: 0.5, y: 0.999999999999))
+    var background = LinearGradient(
+                            gradient:
+                                Gradient(
+                                    stops: [
+                                        .init(color: Color(#colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1)), location: 0),
+                                        .init(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)), location: 1)]),
+                            startPoint: UnitPoint(x: 0.5, y: -3.061671314629196e-17),
+                            endPoint: UnitPoint(x: 0.5, y: 0.999999999999))
     
     
     
     var body: some View {
         ZStack {
             background.ignoresSafeArea()
-            ZStack {
-                
-                //Text("some text").background(GeometryGetter(rect: $rect))
-                // You can then use rect in other places of your view:
-                //Rectangle().frame(width: 100, height: rect.height)
-                
-                EggView()
-                    .offset(x: appear ? -80 : -50, y: appear ? -100 : -180)
-                    .scaleEffect(appear ? 1.5 : 1)
-                    .onAppear  {
-                        withAnimation(.linear(duration: 3)) {
-                            appear = true
-                        }
+            //Text("some text").background(GeometryGetter(rect: $rect))
+            // You can then use rect in other places of your view:
+            //Rectangle().frame(width: 100, height: rect.height)
+            
+            EggView()
+                .offset(x: appear ? -80 : -50, y: appear ? -100 : -180)
+                .scaleEffect(appear ? 1.5 : 1)
+                .onAppear  {
+                    withAnimation(.linear(duration: 3)) {
+                        appear = true
                     }
-                
-                LogInCardView()
-                
-            }
+                }
+            
+            LogInCardView()
+                .offset(x: viewState.width, y: viewState.height)
+                .animation(.spring())
+                .gesture(DragGesture()
+                            .onChanged({ (value) in
+                                        viewState = value.translation})
+                            .onEnded({ (value) in
+                                        viewState = .zero}))
+            
         }
     }
 }
